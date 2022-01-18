0aa142130f918a354059432377913967f181684f
hohuy852 <syhuy851@gmail.com>
2022-01-18 13:50:10 +0700

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,24 +1,47 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faCodeBranch, faStar, faTerminal } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Project = () => {
-    const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        async function getAPI() {
-            let response = await axios.get('https://api.github.com/users/hohuy852/repos')
-            setData(response.data)
-            setLoading(false)
-            // console.log(response.data)
-        }
-        getAPI()
-    }, [])
-    if (loading) {
-        return (<div>Loading..</div>)
-    }
+    const [data, setData] = useState([
+        {
+            name: 'Vuetify Shop',
+            description: ['This is basic an E-commerce website', 'Using Vuetify as Material Design Framework', 'Using VueJS (Vuex) as front-end framework','Nodejs, MongoDB, Socket communication, etc..'],
+            link: 'https://github.com/hohuy852/VuetifyShop',
+            demo: '',
+        },
+        {
+            name: 'Vuetify Shop Admin',
+            description: ['This is basic an Admin website', 'Using Vuetify as Material Design Framework', 'Using VueJS (Vuex) as front-end framework','Nodejs, MongoDB, Socket communication, etc..'],
+            link: 'https://github.com/hohuy852/AdminVuetifyShop',
+            demo: '',
+        },
+        {
+            name: 'Watchflix',
+            description: ['This is basic an movie website', 'Using TaliwindCSS as CSS Framework', 'Using VueJS (Vuex) as front-end framework','Using AniAPI as external API'],
+            link: 'https://github.com/hohuy852/AnimeMovie',
+            demo: '',
+        },
+        {
+            name: 'Weather App ',
+            description: ['This is basic an weather forecast website', 'Using TaliwindCSS as CSS Framework', 'Using React (Redux) as front-end framework','Using Google News API, OpenWeatherMap API as external API'],
+            link: 'https://github.com/hohuy852/WeatherReact',
+            demo: '',
+        },
+        {
+            name: 'Vue Portfolio',
+            description: ['My first portfolio using Vuetify and Vue'],
+            link: 'https://github.com/hohuy852/WeatherReact',
+            demo: '',
+        },
+        {
+            name: 'React Portfolio',
+            description: ['My second portfolio using TailwindCSS and React'],
+            link: 'https://github.com/hohuy852/WeatherReact',
+            demo: '',
+        },
+    ])
     return (
         <section id='hobbies' className='py-32 bg-[#23263a]' >
             <div className='xl:container px-5'>
@@ -45,7 +68,7 @@ const Project = () => {
                             <div className='grid justify-items-center text-center '>
                                 <span className="text-red-custom uppercase text-sm font-medium my-5">1 week ago</span>
                                 <h2 className='text-2xl text-white mb-6 font-bold '>{repos.name}</h2>
-                                <p className='mb-7 px-3' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis aperiam magni, consectetur veniam exercitationem cum nostrum voluptatibus officiis, a veritatis maxime. Praesentium error iusto hic possimus inventore? Modi, vel suscipit?</p>
+                                {repos.description.map((des => ( <p className='mb-7 px-3 text-left' >{des}</p>)))}
                                 {/* <div className='w-full h-16  rounded-b-lg text-white'>
                                     <div className='flex items-center h-full text-center ' >
                                         <div className='w-full flex  justify-center items-center gap-x-3'>
@@ -69,7 +92,7 @@ const Project = () => {
                                     </div>
                                 </div>
                                 <hr /> */}
-                                <div className='w-5/6 grid gap-y-2 pb-8'>
+                                {/* <div className='w-5/6 grid gap-y-2 pb-8'>
                                     <div className='chart'>
                                         <div className='text-white flex flex-col gap-y-2 w-full'>
                                             <div className='skill relative ' key={index}>
@@ -89,7 +112,7 @@ const Project = () => {
                                         </div>
                                     </div>
                                     <div className='chart'>
-                                        <div className='text-white flex flex-col gap-y-2 w-full'>
+                                        <div className='text-white flex flex-col gap-y-2 w-full'> 
                                             <div className='skill relative ' key={index}>
                                                 <p className='mb-2 text-left'>ABC</p>
                                                 <div style={{ width: '78%' }} className='bg-red-custom relative h-5'>
@@ -97,29 +120,25 @@ const Project = () => {
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </div> */}
                                 <div className='grid grid-cols-2 justify-items-center pb-4'>
-                                    <a href={repos.html_url} target="_blank" rel="noopener noreferrer" className='px-1 lg:px-4 py-2 rounded-xl hover:bg-red-custom'>
+                                    <a href={repos.link} target="_blank" rel="noopener noreferrer" className='px-1 lg:px-4 py-2 rounded-xl hover:bg-red-custom'>
                                         <div className='flex gap-x-3 items-center'>
-                                            <FontAwesomeIcon icon={faGithub}  size='2x'/>
+                                            <FontAwesomeIcon icon={faGithub} size='2x' />
                                             <span>Source Code</span>
                                         </div>
                                     </a>
                                     <a href="/" target="_blank" rel="noopener noreferrer" className='px-8 py-2  rounded-xl hover:bg-red-custom'>
                                         <div className='flex gap-x-3 items-center'>
-                                            <FontAwesomeIcon icon={faTerminal}  size='2x' />
+                                            <FontAwesomeIcon icon={faTerminal} size='2x' />
                                             <span>Demo</span>
                                         </div>
                                     </a>
-
                                 </div>
                             </div>
-
                         </div>))
                     }
                 </div>
-
-
             </div>
         </section>
     )
